6d8b035ecaa179489d60912b661ecbde61c858f8
201902482dh <101381576+201902482dh@users.noreply.github.com>
2022-05-06 21:40:53 +0900

    스택구조 추가

--- 자료구조.py
+++ 자료구조.py
@@ -33,3 +33,24 @@ class Queue:
         self.front = -1
         self.rear = self.size-1
 
+class Stack:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return len(self.items) == 0
+    def clear(self):
+        self.items = []
+    def push(self,e):
+        self.items.append(e)
+    def pop(self):
+        if self.isEmpty():
+            print("Stack is empty")
+        else:
+            return self.items.pop()
+    def size(self):
+        return len(self.items)
+    def peek(self):
+        if self.isEmpty():
+            print("Stack is empty")
+        else:
+            return self.items[-1]
